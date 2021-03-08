dac09e342e96380111edcc6943bbb4d5fea3df10
anderson89marques <andersonoanjo18@gmail.com>
2021-03-07 23:39:01 -0300

    Adiçao de relacionamentos entre os modelos.

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from ..funcionarios.models import Funcionario
 
 class Documento(models.Model):
     nome = models.CharField(max_length=100)
+    autor = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
